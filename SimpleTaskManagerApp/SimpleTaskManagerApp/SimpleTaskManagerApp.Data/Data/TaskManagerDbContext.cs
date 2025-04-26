6e8e632071f27c2cf729dfe720c9a228f136841b
T.Ivanov2304 <t.ivanov2304@gmail.com>
2025-04-26 18:45:57 +0300

    Create the project with Identity and InitialMigration

---
+++ SimpleTaskManagerApp/SimpleTaskManagerApp/SimpleTaskManagerApp.Data/Data/TaskManagerDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SimpleTaskManagerApp.Data
+{
+	public class TaskManagerDbContext : IdentityDbContext
+	{
+		public TaskManagerDbContext(DbContextOptions<TaskManagerDbContext> options)
+			: base(options)
+		{
+		}
+	}
+}
---
+++ SimpleTaskManagerApp/SimpleTaskManagerApp/SimpleTaskManagerApp/Program.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using SimpleTaskManagerApp.Data;
+
+namespace SimpleTaskManagerApp
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
+
+			// Add services to the container.
+			var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+			//Add Postgre
+			builder.Services.AddDbContext<TaskManagerDbContext>(options =>
+				options.UseNpgsql(connectionString));
+
+			builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+			builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+				.AddEntityFrameworkStores<TaskManagerDbContext>();
+			builder.Services.AddControllersWithViews();
+
+			var app = builder.Build();
+
+			// Configure the HTTP request pipeline.
+			if (app.Environment.IsDevelopment())
+			{
+				app.UseMigrationsEndPoint();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Home/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
+
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
+
+			app.UseRouting();
+
+			app.UseAuthorization();
+
+			app.MapControllerRoute(
+				name: "default",
+				pattern: "{controller=Home}/{action=Index}/{id?}");
+			app.MapRazorPages();
+
+			app.Run();
+		}
+	}
+}
