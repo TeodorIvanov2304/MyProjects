410328929d0d056a271aa6bee89c388475098471
T.Ivanov2304 <t.ivanov2304@gmail.com>
2025-04-26 18:56:58 +0300

    Create class libraries Data.Models and Common
    Add class EntityValidationConstants in Common

---
+++ SimpleTaskManagerApp/SimpleTaskManagerApp/SimpleTaskManagerApp.Common/EntityValidationConstants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTaskManagerApp.Common
+{
+	public static class EntityValidationConstants
+	{
+		//Application user
+		public const int ApplicationUserFirstNameMinLength = 2;
+		public const int ApplicationUserFirstNameMaxLength = 250;
+		public const int ApplicationUserLastNameMinLength = 2;
+		public const int ApplicationUserLastNameMaxLength = 250;
+	}
+}
