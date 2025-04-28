fa10b794ef8a015a732d486df7798b5e9870d198
T.Ivanov2304 <t.ivanov2304@gmail.com>
2025-04-28 09:32:45 +0300

    Create ITaskRepositoryInterface

---
+++ SimpleTaskManagerApp/SimpleTaskManagerApp.Data/Data/Repositories/Interfaces/ITaskRepository.cs
@@ -0,0 +1,14 @@
+﻿using SimpleTaskManagerApp.Data.Models.Models;
+
+namespace SimpleTaskManagerApp.Data.Data.Repositories.Interfaces
+{
+	public interface ITaskRepository
+	{
+		Task<IEnumerable<AppTask>> GetAllTasksAsync();
+		Task<AppTask?> GetByIdAsync(Guid id);
+		Task AddAsync(AppTask task);
+		Task UpdateAsync(AppTask task);
+		Task DeleteAsync(Guid id);
+		Task SaveChangesAsync();
+	}
+}
