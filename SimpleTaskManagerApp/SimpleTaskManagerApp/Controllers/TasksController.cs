e247a7b449e7c72152bf2bf8ab0dd376a2de0dd3
T.Ivanov2304 <t.ivanov2304@gmail.com>
2025-04-29 14:58:42 +0300

    Craete TaskController with Index method

---
+++ SimpleTaskManagerApp/SimpleTaskManagerApp/Controllers/TasksController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleTaskManagerApp.Services.Data;
+using SimpleTaskManagerApp.Services.Data.Interfaces;
+
+namespace SimpleTaskManagerApp.Controllers
+{
+	public class TasksController : Controller
+	{
+		private readonly IAppTaskService _appTaskService;
+
+        public TasksController(IAppTaskService appTaskService)
+        {
+            this._appTaskService = appTaskService;
+        }
+
+		[HttpGet]
+        public async Task<IActionResult> Index()
+		{	
+			var model = await _appTaskService.GetAllTasksAsync();
+
+			return View(model);
+		}
+	}
+}
