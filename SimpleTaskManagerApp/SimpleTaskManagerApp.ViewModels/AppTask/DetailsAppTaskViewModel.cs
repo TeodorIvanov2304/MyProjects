3ceb3de5bb861614941f49b491f7f98d9a51c960
T.Ivanov2304 <t.ivanov2304@gmail.com>
2025-05-13 13:36:42 +0300

    Create DetailsAppTaskViewModel.cs

---
+++ SimpleTaskManagerApp/SimpleTaskManagerApp.ViewModels/AppTask/DetailsAppTaskViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace SimpleTaskManagerApp.ViewModels.AppTask
+{
+	public class DetailsAppTaskViewModel
+	{
+		public string Username { get; set; } = null!;
+		public string Title { get; set; } = null!;
+		public string Description { get; set; } = null!;
+		public DateTime CreatedAt { get; set; }
+		public DateTime? DueDate { get; set; }
+		public string StatusName { get; set; } = null!;
+	}
+}
