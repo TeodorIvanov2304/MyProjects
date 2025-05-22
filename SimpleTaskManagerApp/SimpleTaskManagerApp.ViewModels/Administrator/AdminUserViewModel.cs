17e6bd26195321535ece7a85d2117f00f9ff31fb
T.Ivanov2304 <t.ivanov2304@gmail.com>
2025-05-22 19:12:04 +0300

    Create UserController,  create _ViewImports.cshtml and craete AdminUserViewModel

---
+++ SimpleTaskManagerApp/SimpleTaskManagerApp.ViewModels/Administrator/AdminUserViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTaskManagerApp.ViewModels.Administrator
+{
+	public class AdminUserViewModel
+	{
+		public string Email { get; set; } = null!;
+		public string FirstName { get; set; } = null!;
+		public string LastName { get; set; } = null!;
+		public virtual IEnumerable<string> Roles { get; set; } = new HashSet<string>();
+	}
+}
---
+++ SimpleTaskManagerApp/SimpleTaskManagerApp/Areas/Administrator/Controllers/UserController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleTaskManagerApp.Areas.Administrator.Controllers
+{
+	[Area("Administrator")]
+	[Authorize(Roles ="Administrator")]
+	public class UserController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
