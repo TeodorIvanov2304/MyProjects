5851f315cd3f0cc8eeadb5f062eb13c9ef20be99
T.Ivanov2304 <t.ivanov2304@gmail.com>
2025-06-01 22:55:30 +0300

    Create FilterAppTaskViewModel.cs

---
+++ SimpleTaskManagerApp/SimpleTaskManagerApp.ViewModels/AppTask/FilterAppTaskViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace SimpleTaskManagerApp.ViewModels.AppTask
+{
+	public class FilterAppTaskViewModel
+	{
+		public string? TitleKeyword { get; set; }
+
+		public string? CreatedByEmail { get; set; }
+
+		public int? StatusId { get; set; }
+
+		public bool? IsDeleted { get; set; }
+
+		public DateTime? DueDateFrom { get; set; }
+		public DateTime? DueDateTo { get; set; }
+
+		public DateTime? CreatedAtFrom { get; set; }
+		public DateTime? CreatedAtTo { get; set; }
+
+		public string? SortBy { get; set; } 
+		public bool SortDescending { get; set; } = false;
+	}
+}
