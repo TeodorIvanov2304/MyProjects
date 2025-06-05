6ed240ccd0d944914b509934e268860b47ee7b0e
T.Ivanov2304 <t.ivanov2304@gmail.com>
2025-06-05 22:00:38 +0300

    Create FilterAppTaskViewModelUser.cs

---
+++ SimpleTaskManagerApp/SimpleTaskManagerApp.ViewModels/AppTask/FilterAppTaskViewModelUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTaskManagerApp.ViewModels.AppTask
+{
+	public class FilterAppTaskViewModelUser
+	{
+		public string? TitleKeyword { get; set; }
+		public DateTime? CreatedAtFrom { get; set; }
+		public DateTime? CreatedAtTo { get; set; }
+		public DateTime? DueDateFrom { get; set; }
+		public DateTime? DueDateTo { get; set; }
+		public int? StatusId { get; set; }
+	}
+}
