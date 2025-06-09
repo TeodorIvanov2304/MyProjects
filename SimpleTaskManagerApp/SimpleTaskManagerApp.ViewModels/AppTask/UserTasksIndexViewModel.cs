c5651bf1082010daa5a0043bbb18f9c157924b01
T.Ivanov2304 <t.ivanov2304@gmail.com>
2025-06-09 13:44:53 +0300

    Create UserTasksIndexViewModel.cs

---
+++ SimpleTaskManagerApp/SimpleTaskManagerApp.ViewModels/AppTask/UserTasksIndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTaskManagerApp.ViewModels.AppTask
+{
+	public class UserTasksIndexViewModel
+	{
+		public FilterAppTaskViewModelUser Filter { get; set; } = new();
+		public IEnumerable<AppTaskListViewModel> Tasks { get; set; } = new List<AppTaskListViewModel>();
+	}
+}
