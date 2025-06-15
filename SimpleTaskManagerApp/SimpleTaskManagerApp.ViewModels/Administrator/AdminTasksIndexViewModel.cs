ceafa969642e3ca1ead5ad8bc8c905ff55547d61
T.Ivanov2304 <t.ivanov2304@gmail.com>
2025-06-15 21:43:29 +0300

    Update AdminTasksIndexViewModel.cs

--- SimpleTaskManagerApp/SimpleTaskManagerApp.ViewModels/Administrator/AdminTasksIndexViewModel.cs
+++ SimpleTaskManagerApp/SimpleTaskManagerApp.ViewModels/Administrator/AdminTasksIndexViewModel.cs
@@ -7,5 +7,9 @@ namespace SimpleTaskManagerApp.ViewModels.Administrator
 		public IEnumerable<AdminTaskViewModel> Tasks { get; set; } = new List<AdminTaskViewModel>();
 		public FilterAppTaskViewModelAdmin Filter { get; set; } = new FilterAppTaskViewModelAdmin();
 		public IEnumerable<SelectListItem> Statuses { get; set; } = new List<SelectListItem>();
+
+		//For pagination
+		public int CurrentPage { get; set; }
+		public int TotalPages { get; set; }
 	}
 }
