5eaec5ac121127e2c7e0d2072eb94069bf0167f7
T.Ivanov2304 <t.ivanov2304@gmail.com>
2025-06-18 23:28:09 +0300

    Create AdminUsersIndexViewModel.cs

---
+++ SimpleTaskManagerApp/SimpleTaskManagerApp.ViewModels/Administrator/AdminUsersIndexViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleTaskManagerApp.ViewModels.Administrator
+{
+	public class AdminUsersIndexViewModel
+	{
+		public IEnumerable<AdminUserViewModel> Users { get; set; } = new List<AdminUserViewModel>();
+		public FilterUserViewModelAdmin Filter { get; set; } = new();
+		public int CurrentPage { get; set; }
+		public int TotalPages { get; set; }
+	}
+}
