6d85f6ba9f566145667d5b181370f5618580d4ef
T.Ivanov2304 <t.ivanov2304@gmail.com>
2025-07-09 12:16:35 +0300

    Create ILogEntry interface

---
+++ SimpleTaskManagerApp/SimpleTaskManagerApp.Services.Data/Interfaces/ILogEntry.cs
@@ -0,0 +1,7 @@
+﻿namespace SimpleTaskManagerApp.Services.Data.Interfaces
+{
+	public interface ILogEntry
+	{
+		Task LogAsync(string userId, string userEmail, string action, string entityType, string? entityId = null);
+	}
+}
