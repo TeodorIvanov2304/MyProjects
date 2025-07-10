6d0deb388365ebb632cb659c9f96667964fcfa25
T.Ivanov2304 <t.ivanov2304@gmail.com>
2025-07-10 09:02:54 +0300

    Create LogsController.cs in Administrator area, create Index method

---
+++ SimpleTaskManagerApp/SimpleTaskManagerApp/Areas/Administrator/Controllers/LogsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SimpleTaskManagerApp.Data;
+
+namespace SimpleTaskManagerApp.Areas.Administrator.Controllers
+{
+	[Area("Administrator")]
+	[Authorize(Roles = "Administrator")]
+	public class LogsController : Controller
+	{
+		private readonly TaskManagerDbContext _context;
+
+        public LogsController(TaskManagerDbContext context)
+        {
+            this._context = context;
+        }
+        public async Task<IActionResult> Index()
+		{
+			var logs = await _context.LogEntries
+				.OrderByDescending(l => l.TimeStamp)
+				.Take(100)
+				.ToListAsync();
+
+			return View(logs);
+		}
+	}
+}
