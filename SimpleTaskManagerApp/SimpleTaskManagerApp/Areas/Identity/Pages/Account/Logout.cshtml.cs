140d16dffe4e7c187cb3e1a0228a5b338737aef0
T.Ivanov2304 <t.ivanov2304@gmail.com>
2025-07-19 10:09:37 +0300

    Add LogEntry to Logout.cshtml.cs

--- SimpleTaskManagerApp/SimpleTaskManagerApp/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ SimpleTaskManagerApp/SimpleTaskManagerApp/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using SimpleTaskManagerApp.Data.Models.Models;
+using SimpleTaskManagerApp.Services.Data.Interfaces;
 
 namespace SimpleTaskManagerApp.Areas.Identity.Pages.Account
 {
@@ -13,27 +14,39 @@ namespace SimpleTaskManagerApp.Areas.Identity.Pages.Account
     {
         private readonly SignInManager<ApplicationUser> _signInManager;
         private readonly ILogger<LogoutModel> _logger;
-
-        public LogoutModel(SignInManager<ApplicationUser> signInManager, ILogger<LogoutModel> logger)
+		private readonly UserManager<ApplicationUser> _userManager;
+		private readonly ILogEntryService _logService;
+		public LogoutModel(SignInManager<ApplicationUser> signInManager, ILogger<LogoutModel> logger, UserManager<ApplicationUser> userManager, ILogEntryService logService)
         {
             _signInManager = signInManager;
             _logger = logger;
+			_userManager = userManager;
+			_logService = logService;
         }
 
-        public async Task<IActionResult> OnPost(string returnUrl = null)
-        {
-            await _signInManager.SignOutAsync();
-            _logger.LogInformation("User logged out.");
-            if (returnUrl != null)
-            {
-                return LocalRedirect(returnUrl);
-            }
-            else
-            {
-                // This needs to be a redirect so that the browser performs a new
-                // request and the identity for the user gets updated.
-                return RedirectToPage();
-            }
-        }
-    }
+		public async Task<IActionResult> OnPost(string returnUrl = null)
+		{
+
+			var user = await _userManager.GetUserAsync(User);
+
+			if (user != null)
+			{
+				await _logService.LogAsync(user.Id, user.Email ?? "Unknown", "User logged out", "Authentication", user.UserName);
+			}
+
+			await _signInManager.SignOutAsync();
+			_logger.LogInformation("User logged out.");
+
+			if (returnUrl != null)
+			{
+				return LocalRedirect(returnUrl);
+			}
+			else
+			{
+				// This needs to be a redirect so that the browser performs a new
+				// request and the identity for the user gets updated.
+				return RedirectToPage();
+			}
+		}
+	}
 }
