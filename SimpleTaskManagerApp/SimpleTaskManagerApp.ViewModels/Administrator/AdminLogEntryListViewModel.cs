d1fa5a425559767a2b1f031b2ba452b42485eef4
T.Ivanov2304 <t.ivanov2304@gmail.com>
2025-07-21 16:49:30 +0300

    Create LogEntryViewModel and AdminLogEntryListViewModel

---
+++ SimpleTaskManagerApp/SimpleTaskManagerApp.ViewModels/Administrator/AdminLogEntryListViewModel.cs
@@ -0,0 +1,13 @@
+﻿
+
+using SimpleTaskManagerApp.ViewModels.LogEntry;
+
+namespace SimpleTaskManagerApp.ViewModels.Administrator
+{
+	public class AdminLogEntryListViewModel
+	{
+		public int CurrentPage { get; set; }
+		public int TotalPages { get; set; }
+		public IEnumerable<LogEntryViewModel> Logs { get; set; } = new List<LogEntryViewModel>();
+	}
+}
---
+++ SimpleTaskManagerApp/SimpleTaskManagerApp.ViewModels/LogEntry/LogEntryViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace SimpleTaskManagerApp.ViewModels.LogEntry
+{
+	public class LogEntryViewModel
+	{
+		public string UserEmail { get; set; } = string.Empty;
+		public string Action { get; set; } = string.Empty;
+		public string EntityType { get; set; } = string.Empty;
+		public string? EntityName { get; set; }
+		public DateTime TimeStamp { get; set; }
+	}
+}
